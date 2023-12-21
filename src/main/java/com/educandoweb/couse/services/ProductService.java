174d7f9aa34ad8a5a041f91d2e2bc7225d9c0b72
Cesar <cesar.socin@gmail.com>
2023-12-21 16:14:13 -0200

    Product

--- src/main/java/com/educandoweb/couse/services/ProductService.java
+++ src/main/java/com/educandoweb/couse/services/ProductService.java
@@ -6,21 +6,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.educandoweb.couse.entities.Order;
-import com.educandoweb.couse.repositories.OrderRepository;
+import com.educandoweb.couse.entities.Product;
+import com.educandoweb.couse.repositories.ProductRepository;
 
 @Service
-public class OrderService {
+public class ProductService {
 
 	@Autowired
-	private OrderRepository repository;
+	private ProductRepository repository;
 	
-	public List<Order> findAll() {
+	public List<Product> findAll() {
 		return repository.findAll(); 
 	}
 	
-	public Order findById(Long id) {
-		Optional<Order> obj = repository.findById(id);
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
 		return obj.get();
 	}
 }
